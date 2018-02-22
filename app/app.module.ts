f43f77dc883e0485efcfc15abc2ab77502fc76b5
Igor Busquets <igor.busquets@altran.com>
2018-02-22 12:21:37 +0000

    Created first service and injected into artist-list.component.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,4 @@
-
+//imports of internal dependencies
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser' 
 
@@ -7,10 +7,13 @@ import { MskAppComponent } from './msk-app.component'
 import { ArtistsListComponent } from './artists/artists-list/artists-list.component'
 import { ArtistThumbnailComponent } from './artists/artist-thumbnail/artist-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component';
+import { ArtistService } from './artists/shared/artist.service';
+
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [MskAppComponent, ArtistsListComponent, ArtistThumbnailComponent, NavBarComponent],
+    providers: [ArtistService],
     bootstrap: [MskAppComponent]
 })
 export class AppModule{
---
+++ app/artists/shared/artist.service.ts
@@ -0,0 +1,249 @@
+import { Injectable } from "@angular/core";
+
+// required only if this service requires injecting other services here
+// but is good practice to always add Injectable decorator in services
+@Injectable()
+
+export class ArtistService{
+    getArtists(){
+        return ARTISTS;
+    }
+}
+
+//mock data to be added in database / API
+const ARTISTS = [
+    {
+        id: 1,
+        name: 'Name One',
+        genre: 'Some genre',
+        description: 'Being a developer in 2037 is about more than just writing bug-free code. You also have to look the part.',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 2,
+        name: 'Name Twott3te rtert eetert er tertert erterterte rwwueiwerwerwerweriwerwer',
+        genre: 'Some genre',
+        description: 'Coinciding with the release of Star Wars Episode 18, this talk will show how to use directives in your Angular 4 development',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 3,
+        name: 'Name Three',
+        genre: 'Some genre',
+        description: 'Sometimes our internal biases can make it difficult forthese well-designed coworkers to really feel at home coding alongside us.',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 4,
+        name: 'Name Four',
+        genre: 'Some genre',
+        description: 'Angular has been used in most of the major peace brokering that has happened in the last decade',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 5,
+        name: 'Name Five',
+        genre: 'Some genre',
+        description: 'We all know that Angular is used in most waiter-bots and coke vending machines, but did you know that was also used to write the core engine in the majority of voting machines? ',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 6,
+        name: 'Name Six',
+        genre: 'Some genre',
+        description: 'Let\'s take a look at some of the stranger pieces of Angular 4, including neural net nets, Android in Androids, and using pipes with actual pipes.',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 7,
+        name: 'Name Seven',
+        genre: 'Some genre',
+        description: 'Being a developer in 2037 is about more than just writing bug-free code. You also have to look the part.',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 8,
+        name: 'Name Eight',
+        genre: 'Some genre',
+        description: 'Coinciding with the release of Star Wars Episode 18, this talk will show how to use directives in your Angular 4 development',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 9,
+        name: 'Name Nine',
+        genre: 'Some genre',
+        description: 'Yes, we all work with cyborgs and androids and Martians, but we probably don\'t realize that sometimes our internal biases can make it difficult forthese well-designed coworkers to really feel at home coding alongside us.',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 10,
+        name: 'Name Ten',
+        genre: 'Some genre',
+        description: 'Angular has been used in most of the major peace brokering that has happened in the last decade',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 11,
+        name: 'Name Eleven',
+        genre: 'Some genre',
+        description: 'No, this talk isn\'t about slot machines. ',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 12,
+        name: 'Name Twelve',
+        genre: 'Some genre',
+        description: 'Let\'s take a look at some of the stranger pieces of Angular 4, including neural net nets, Android in Androids, and using pipes with actual pipes.',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        },
+    },
+    {
+        id: 13,
+        name: 'Name Thirteen',
+        genre: 'Some genre',
+        country: 'Portugal',
+        user: 'user1',
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        songs: {
+            song1: 'Song1 name',
+            song2: 'Song2 name',
+            song3: 'Song3 name',
+        },
+        links: {
+            link1: 'www.google.com',
+            link2: 'www.wikipedia.org'
+        }
+    }
+    
+]
+//end of mock data array
