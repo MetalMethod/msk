14ee79579a5ce95ad3234ade1639f5e877f97480
Igor Busquets <igor.busquets@altran.com>
2018-03-02 13:01:09 +0000

    Login form created, values are passed to component method

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import {userRoutes} from './user.routes';
+import  {userRoutes } from './user.routes';
+
+import { FormsModule } from '@angular/forms';
 
 import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     imports:[
         BrowserModule,
-        RouterModule.forChild(userRoutes)
+        RouterModule.forChild(userRoutes),
+        FormsModule
     ],
     declarations: [
-        ProfileComponent
+        ProfileComponent,
+        LoginComponent
     ],
     providers: [
 
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,7 @@
 import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './login/login.component';
 
 export const userRoutes = [
     { path: 'user/profile',  component: ProfileComponent },
-    { path: 'user',  component: ProfileComponent }
+    { path: 'user/login',  component: LoginComponent }
 ]
